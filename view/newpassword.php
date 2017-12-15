0552ee289806161bd091c03f2d877e6268c4808e
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2017-12-15 18:37:03 +0100

    last changes for today

--- view/newpassword.php
+++ view/newpassword.php
@@ -17,11 +17,11 @@ require_once "header.php";
 <body>
 <div class="login-page">
     <div class="form">
-    <h1>Reset Password</h1>
+    <h1>Enter new password</h1>
         <form method="post" class="login-form">
             <input type="hidden" name="token"value="<?php echo $this->token ?>">
             <input type="password" name="password" placeholder="password">
-            <input type="password" name="repeatpassword" placeholder="repeat">
+            <input type="password" name="repeatpassword" placeholder="password (repeat)">
             <?php if (isset($this->errormsg)): ?>
                 <p class="message" style="color: red"><?php echo $this->errormsg ?></p>
             <?php endif ?>
