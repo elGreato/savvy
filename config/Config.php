ed4397b43c9ebfb21066dbc71acb2b685c66aae9
Kevin Neuschwander <kevin.neuschwander@students.fhnw.ch>
2017-12-15 19:30:00 +0100

    push for testing heroku

--- config/Config.php
+++ config/Config.php
@@ -28,7 +28,7 @@ class Config
                 self::$config["pdo"]["password"] = $dbopts["pass"];
             }
             if (isset($_ENV["API_KEY"])) {
-                self::$config["email"]["apikey"] = getenv('EMAIL');
+                self::$config["email"]["apikey"] = getenv('API_KEY');
 
             }
         }
