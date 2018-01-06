613be5bfc82438675676a459baadceaf9cd6a5d5
Hyp-nos <ali.habbabeh@hotmail.com>
2018-01-06 12:25:04 +0100

    deleted some unused stuff

--- index.php
+++ index.php
@@ -255,7 +255,6 @@ try {
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.textcomplete/1.8.0/jquery.textcomplete.js"></script>
 <script type="text/javascript" src="view/assets/js/jquery-comments.js"></script>
-<!-- Data to be deleted later Ali-->
-<script type="text/javascript" src="data/comments-data.js"></script>
+
 
 
